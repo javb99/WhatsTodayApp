680ffcd6440027054ffdb6e56fa3e24325dccc7c
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2018-08-05 16:37:38 -0700

    Initial Commit

---
+++ WhatsToday/CoreApp/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  WhatsToday
+//
+//  Created by Joseph Van Boxtel on 8/5/18.
+//  Copyright © 2018 Joseph Van Boxtel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
